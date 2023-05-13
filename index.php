e51135183b45cec46f7b289ce9f6c44ad53041c3
magedalward <91756551+magedalward@users.noreply.github.com>
2023-05-13 11:41:48 +0200

    project structure was established

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include './includes/header.php';
+?>
+
+<link rel="stylesheet" href="shared/css/style.css">
+</head>
+<body>
+
+<!-- Here is The start of the body:index.php --------------------------------------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src=".shard/index.js"></script>
+<?php
+include './includes/footer.php';
+?>
